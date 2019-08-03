5c899c6dc49f8564bf63ed1197526fda37fffd92
kclivingood <42717009+kclivingood@users.noreply.github.com>
2019-08-03 01:53:35 -0400

    rwd and cleanup

--- scripts.js
+++ scripts.js
@@ -1,8 +1,4 @@
-const arrCapitalize = function (string) {
-    return string.charAt(0).toUpperCase() + string.slice(1)
-}
-
-const test = document.querySelector('#palindrome-form').addEventListener('submit', function (e) {
+document.querySelector('#palindrome-form').addEventListener('submit', function (e) {
     e.preventDefault()
     // clears these elements each time the function is ran
     document.querySelector('#validation').innerText = ''
@@ -14,7 +10,9 @@ const test = document.querySelector('#palindrome-form').addEventListener('submit
     let b = arr.length - 1
 
     // loops through the array and checks both sides incrementally
-    if (arr.length > 2) {
+    if (arr.length > 20) {
+        document.querySelector('#validation').innerText = 'Your cannot exceed 20 letters'
+    } else if (arr.length > 2) {
         for (let a = 0; a < Math.floor(arr.length / 2); a++) {   
             if (arr[a] === arr[b]) {
                 b--
@@ -26,8 +24,9 @@ const test = document.querySelector('#palindrome-form').addEventListener('submit
         if (count = Math.floor(arr.length / 2)) {
             document.querySelector('#palindrome').innerText = `"${message}" is a palindrome!`
         }
+        
     } else {
-        document.querySelector('#validation').innerText = 'You must enter more than two letters in order to check for a palindrome'
+        document.querySelector('#validation').innerText = 'You must enter more than two letters'
     }
     e.target.elements.text.value = ''
     })
